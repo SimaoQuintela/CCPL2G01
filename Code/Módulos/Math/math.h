25b0ef77e43b751504f6ba31f091fb30de216ea1
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2021-03-23 22:27:38 +0000

    Add files via upload

---
+++ Code/Módulos/Math/math.h
@@ -0,0 +1,39 @@
+#include "stack.h"
+void soma(STACK *s);
+
+
+void subtrai(STACK *s);
+
+
+void multiplica(STACK *s);
+    
+
+void dividir(STACK *s);
+  
+
+void decrementa(STACK *s);
+
+
+void incrementa(STACK *s);
+  
+
+void modulo(STACK *s);
+
+
+void expoente(STACK *s);
+
+
+void e(STACK *s); 
+
+
+void ou(STACK *s);
+  
+
+void xorr(STACK *s);
+  
+
+
+void nott(STACK *s);
+
+
+   

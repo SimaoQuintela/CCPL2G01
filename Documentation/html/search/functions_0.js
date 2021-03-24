ad574140d42f890dfaf45dcf5d6f515c935349c8
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-03-24 23:54:52 +0000

    Delete Documentation directory

--- Documentation/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['decrementa_37',['decrementa',['../maths_8c.html#a954e9383095a145d1cb6a1a2d26aa451',1,'decrementa(STACK *s):&#160;maths.c'],['../maths_8h.html#a954e9383095a145d1cb6a1a2d26aa451',1,'decrementa(STACK *s):&#160;maths.c']]],
-  ['dividir_38',['dividir',['../maths_8c.html#a9d9afcdfcf47ca08bcd8b31031871b06',1,'dividir(STACK *s):&#160;maths.c'],['../maths_8h.html#a9d9afcdfcf47ca08bcd8b31031871b06',1,'dividir(STACK *s):&#160;maths.c']]]
-];
--- Documentation/html/search/functions_3.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['main_42',['main',['../main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.c']]],
-  ['modulo_43',['modulo',['../maths_8c.html#ac47c510fef88bce38e48b07b6017c54e',1,'modulo(STACK *s):&#160;maths.c'],['../maths_8h.html#ac47c510fef88bce38e48b07b6017c54e',1,'modulo(STACK *s):&#160;maths.c']]],
-  ['multiplica_44',['multiplica',['../maths_8c.html#a1a83e81a0989cfe6e96090f7fc2d2198',1,'multiplica(STACK *s):&#160;maths.c'],['../maths_8h.html#a1a83e81a0989cfe6e96090f7fc2d2198',1,'multiplica(STACK *s):&#160;maths.c']]]
-];

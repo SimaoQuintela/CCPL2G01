fd249e8e2cd63a0d3a38b9e5f17bda3fdad54f51
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-03-25 00:02:30 +0000

    Add files via upload

---
+++ Documentation/html/search/all_9.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['soma_23',['soma',['../maths_8c.html#ab036ce08365356af3d41ddf51b84d4b6',1,'soma(STACK *s):&#160;maths.c'],['../maths_8h.html#ab036ce08365356af3d41ddf51b84d4b6',1,'soma(STACK *s):&#160;maths.c']]],
+  ['stack_24',['STACK',['../structSTACK.html',1,'']]],
+  ['stack_2ec_25',['stack.c',['../stack_8c.html',1,'']]],
+  ['stack_2eh_26',['stack.h',['../stack_8h.html',1,'']]],
+  ['subtrai_27',['subtrai',['../maths_8c.html#a90cd2dffdaa4518db18fc9804904e16e',1,'subtrai(STACK *s):&#160;maths.c'],['../maths_8h.html#a90cd2dffdaa4518db18fc9804904e16e',1,'subtrai(STACK *s):&#160;maths.c']]]
+];

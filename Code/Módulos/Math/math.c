d9fea545fa221debeac3ac77a8890ff2de948c53
Nuno Costa <75766357+Nuno23C@users.noreply.github.com>
2021-04-08 10:06:49 +0100

    Update math.c

--- Code/Módulos/Math/math.c
+++ Code/Módulos/Math/math.c
@@ -10,48 +10,279 @@
 #include "maths.h"
 #include "stack.h"
 
-/**
-* \author Hugo Rocha
-* \brief Função soma - Função responsável por fazer a soma.
-*/
+
+// Função soma
 void soma(STACK *s){
-    long X = POP(s);
-    long Y = POP(s);
-    PUSH(s, X + Y);
+    // X argument 
+    if(has_type(top(s), LONG) && has_type(penultimo(s), LONG)){
+        long x = pop_LONG(s);       // x long
+        long y = pop_LONG(s);       // y long
+
+        push_LONG(s, x+y);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), DOUBLE)){
+        double x = pop_DOUBLE(s);   // x double
+        double y = pop_DOUBLE(s);   // y double
+
+        push_DOUBLE(s, x+y);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), DOUBLE)){
+        long x = pop_LONG(s);       // x long
+        double y = pop_DOUBLE(s);   // y double
+
+        double result = (double)x + y;  
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), LONG)){
+        double x = pop_DOUBLE(s);   // x double
+        long y =  pop_LONG(s);      // y long
+
+        double result = x + (double)y;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), CHAR)){
+        char x = pop_CHAR(s);       // x char
+        char y = pop_CHAR(s);       // y char
+
+        long result = (long)x + (long)y;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), LONG)){
+        char x = pop_CHAR(s);       // x char
+        long y = pop_LONG(s);       // y long 
+
+        long result = (long)x + y;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), CHAR)){
+        long x = pop_LONG(s);       // x long
+        char y = pop_CHAR(s);       // y char
+
+        long result = x + (long)y;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), DOUBLE)){
+        char x = pop_CHAR(s);       // x char 
+        double y = pop_DOUBLE(s);   // y double
+
+        double result = (double)x + y;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), CHAR)){
+        double x = pop_DOUBLE(s);   // x double 
+        char y = pop_CHAR(s);       // y char
+
+        double result = x + (double)y;
+        push_DOUBLE(s, result);
+    }
 }
 
 
-/**
-* \author Hugo Rocha
-* \brief Função subtrai - Função responsável por fazer a subtração entre dois números.
-*/
+// função subtração
 void subtrai(STACK *s){
-    long X = POP(s);
-    long Y = POP(s);
-    PUSH(s, Y - X);
+
+if(has_type(top(s), LONG) && has_type(penultimo(s), LONG)){
+        long x = pop_LONG(s);       // x long
+        long y = pop_LONG(s);       // y long
+
+        push_LONG(s, y-x);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), DOUBLE)){
+        double x = pop_DOUBLE(s);   // x double
+        double y = pop_DOUBLE(s);   // y double
+
+        push_DOUBLE(s, y-x);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), DOUBLE)){
+        long x = pop_LONG(s);       // x long
+        double y = pop_DOUBLE(s);   // y double
+
+        double result = y - (double)x ;  
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), LONG)){
+        double x = pop_DOUBLE(s);   // x double
+        long y =  pop_LONG(s);      // y long
+
+        double result = (double)y - x;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), CHAR)){
+        char x = pop_CHAR(s);       // x char
+        char y = pop_CHAR(s);       // y char
+
+        long result = (long)y - (long)x;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), LONG)){
+        char x = pop_CHAR(s);       // x char
+        long y = pop_LONG(s);       // y long 
+
+        long result = y- (long)x;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), CHAR)){
+        long x = pop_LONG(s);       // x long
+        char y = pop_CHAR(s);       // y char
+
+        long result = (long)y - x;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), DOUBLE)){
+        char x = pop_CHAR(s);       // x char 
+        double y = pop_DOUBLE(s);   // y double
+
+        double result =  y - (double)x;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), CHAR)){
+        double x = pop_DOUBLE(s);   // x double 
+        char y = pop_CHAR(s);       // y char
+
+        double result = (double)y - x;
+        push_DOUBLE(s, result);
+    }
 }
 
 
-/**
-* \author Hugo Rocha
-* \brief Função multiplica - Função responsável por fazer a multiplicação entre dois números.
-*/
+// função multiplicação
 void multiplica(STACK *s){
-    long X = POP(s);
-    long Y = POP(s);
-    PUSH(s, X * Y);
+    if(has_type(top(s), LONG) && has_type(penultimo(s), LONG)){
+        long x = pop_LONG(s);       // x long
+        long y = pop_LONG(s);       // y long
+
+        push_LONG(s, x*y);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), DOUBLE)){
+        double x = pop_DOUBLE(s);   // x double
+        double y = pop_DOUBLE(s);   // y double
+
+        push_DOUBLE(s, x*y);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), DOUBLE)){
+        long x = pop_LONG(s);       // x long
+        double y = pop_DOUBLE(s);   // y double
+
+        double result = (double)x * y;  
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), LONG)){
+        double x = pop_DOUBLE(s);   // x double
+        long y =  pop_LONG(s);      // y long
+
+        double result = x * (double)y;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), CHAR)){
+        char x = pop_CHAR(s);       // x char
+        char y = pop_CHAR(s);       // y char
+
+        long result = (long)x * (long)y;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), LONG)){
+        char x = pop_CHAR(s);       // x char
+        long y = pop_LONG(s);       // y long 
+
+        long result = (long)x * y;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), CHAR)){
+        long x = pop_LONG(s);       // x long
+        char y = pop_CHAR(s);       // y char
+
+        long result = x * (long)y;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), DOUBLE)){
+        char x = pop_CHAR(s);       // x char 
+        double y = pop_DOUBLE(s);   // y double
+
+        double result = (double)x * y;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), CHAR)){
+        double x = pop_DOUBLE(s);   // x double 
+        char y = pop_CHAR(s);       // y char
+
+        double result = x * (double)y;
+        push_DOUBLE(s, result);
+    }
 }
 
 
-/**
-* \author Hugo Rocha
-* \brief Função dividir - Função responsável por fazer a divisão inteira entre dois números, garantindo que X é diferente de zero.
-*/
+// função divisão
 void dividir(STACK *s){
-    long X = POP(s);
-    assert(X != 0);
-    long Y = POP(s);
-    PUSH(s, Y / X);
+ if(has_type(top(s), LONG) && has_type(penultimo(s), LONG)){
+        long x = pop_LONG(s);       // x long
+        long y = pop_LONG(s);       // y long
+        assert(x != 0);
+
+        push_LONG(s, y / x);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), DOUBLE)){
+        double x = pop_DOUBLE(s);   // x double
+        double y = pop_DOUBLE(s);   // y double
+        assert(x != 0);             
+
+        push_DOUBLE(s, y / x);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), DOUBLE)){
+        long x = pop_LONG(s);       // x long
+        double y = pop_DOUBLE(s);   // y double
+        assert(x != 0);  
+
+        double result = (double)y / x;  
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), LONG)){
+        double x = pop_DOUBLE(s);   // x double
+        long y =  pop_LONG(s);      // y long
+        assert(x != 0);     
+
+        double result = y / (double)x;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), CHAR)){
+        char x = pop_CHAR(s);       // x char
+        char y = pop_CHAR(s);       // y char
+        assert(x != 0);       
+
+        long result = (long)y / (long)x;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), LONG)){
+        char x = pop_CHAR(s);       // x char
+        long y = pop_LONG(s);       // y long
+        assert(x != 0);       
+
+        long result = (long)y / x;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), LONG) && has_type(penultimo(s), CHAR)){
+        long x = pop_LONG(s);       // x long
+        char y = pop_CHAR(s);       // y char
+        assert(x != 0);   
+
+        long result = y / (long)x;
+        push_LONG(s, result);
+
+    } else if(has_type(top(s), CHAR) && has_type(penultimo(s), DOUBLE)){
+        char x = pop_CHAR(s);       // x char 
+        double y = pop_DOUBLE(s);   // y double
+        assert(x != 0);   
+
+        double result = (double)y / x;
+        push_DOUBLE(s, result);
+
+    } else if(has_type(top(s), DOUBLE) && has_type(penultimo(s), CHAR)){
+        double x = pop_DOUBLE(s);   // x double 
+        char y = pop_CHAR(s);       // y char   
+        assert(x != 0);
+
+        double result = y / (double)x;
+        push_DOUBLE(s, result);
+    }
 }
 
 

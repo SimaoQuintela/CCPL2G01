b6372eb79aba2e4e5b46c7f186d02109e5849926
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-11 20:39:03 +0100

    Update parser.c

--- Code/Parser/parser.c
+++ Code/Parser/parser.c
@@ -1,5 +1,7 @@
+/** @file parser Módulo que contém o parser, que tem como objetivo separar uma string token a token e envia-la para o tokenizador para interpretar os vários tokens.
+* \author Simao Quintela
+*/
 #include <string.h>
-//#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,7 +11,12 @@
 #include "tokenizador.h"
 #include "manipulacao_da_stack.h"
 
-// função genérica
+/**
+* \brief A função parse recebe uma stack e uma linha (vindas da main), percorre essa linha separa em token e envia-os um a um para o tokenizador que os vai interpretar.
+* \brief Saindo do ciclo for, a função print_stack imprime a stack.
+* @param delims Servem para delimitar a seleção dos tokens.
+* @param strtok_r Usado para separar a string em tokens e, com o uso desta "extensão" à função strtok não há problema quanto a chamar mais que uma vez esta função.
+*/
 void parse(STACK *s, char *line) {
   char *delims = " \t\n";
   char *token;
@@ -21,7 +28,11 @@ void parse(STACK *s, char *line) {
   print_stack(s);
 }
 
-// função utilizada no comando l 
+/**
+* \brief A função parse2 está responsável por ler linhas subsequentes à primeira 
+* @param delims Servem para delimitar a seleção dos tokens.
+* @param strtok_r Usado para separar a string em tokens e, com o uso desta "extensão" à função strtok não há problema quanto a chamar mais que uma vez esta função.
+*/
 void parse2(STACK *s, char *line) {
   char *delims = " \t\n";
   char *token;

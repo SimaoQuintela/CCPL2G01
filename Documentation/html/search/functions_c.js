e1ffe405950850833ac3c158269f040476dcf767
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-14 18:17:28 +0100

    Add files via upload

---
+++ Documentation/html/search/functions_c.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['tokenizador_98',['tokenizador',['../tokenizador_8c.html#aec91f88790c0adf5bc0311775b8302b9',1,'tokenizador(STACK *s, char *token):&#160;tokenizador.c'],['../tokenizador_8h.html#aec91f88790c0adf5bc0311775b8302b9',1,'tokenizador(STACK *s, char *token):&#160;tokenizador.c']]],
+  ['top_99',['top',['../stack_8c.html#a1c8a0d720a37d9d2a5844e6013c915c2',1,'top(STACK *s):&#160;stack.c'],['../stack_8h.html#a1c8a0d720a37d9d2a5844e6013c915c2',1,'top(STACK *s):&#160;stack.c']]],
+  ['trocadois_100',['trocadois',['../manipulacao__da__stack_8c.html#ad2c846231efe7651bd7edec4f3c7d1f9',1,'trocadois(STACK *s):&#160;manipulacao_da_stack.c'],['../manipulacao__da__stack_8h.html#ad2c846231efe7651bd7edec4f3c7d1f9',1,'trocadois(STACK *s):&#160;manipulacao_da_stack.c']]]
+];

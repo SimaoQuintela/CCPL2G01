0afa6e91eb42b2f0cab3b92483cc87c0df6d1149
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-14 12:31:53 +0100

    Update main.c

--- Code/Main/main.c
+++ Code/Main/main.c
@@ -6,8 +6,11 @@
 #include "parser.h"
 #include "stack.h"
 
+/**
+*	\brief Função principal do programa responsável por inicializar a stack e passar a stack e uma linha para a função parse.
+*/
 int main() {
-  char buf[BUFSIZ];
+  char buf[BUFSIZ];       // linha que vai ser lida, com o tamanho BUFSIZ(macro pré-definida)
   STACK *s = new_stack();
   
   parse(s, fgets(buf, BUFSIZ, stdin));

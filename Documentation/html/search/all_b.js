6615b33286b9bf32f89c172471028105f09fa742
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-14 12:29:35 +0100

    Add files via upload

---
+++ Documentation/html/search/all_b.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['size_39',['size',['../structstack.html#a926a597bae913d1bf4772be35c14b71e',1,'stack']]],
+  ['soma_40',['soma',['../maths_8c.html#ab036ce08365356af3d41ddf51b84d4b6',1,'soma(STACK *s):&#160;maths.c'],['../maths_8h.html#ab036ce08365356af3d41ddf51b84d4b6',1,'soma(STACK *s):&#160;maths.c']]],
+  ['stack_41',['stack',['../structstack.html',1,'stack'],['../structstack.html#ac18b7057f7be1bcabbfe91eaf98e071f',1,'stack::stack()'],['../stack_8h.html#ade8bb1f37e07eb511f3873082665d739',1,'STACK():&#160;stack.h']]],
+  ['stack_2ec_42',['stack.c',['../stack_8c.html',1,'']]],
+  ['stack_2eh_43',['stack.h',['../stack_8h.html',1,'']]],
+  ['stack_5foperation_44',['STACK_OPERATION',['../stack_8c.html#a9903082d211359e185d2b31f64dc02bd',1,'stack.c']]],
+  ['stack_5foperation_5fproto_45',['STACK_OPERATION_PROTO',['../stack_8h.html#aa7e195e1283260f6225089d9c6e4f184',1,'stack.h']]],
+  ['string_46',['STRING',['../structdata.html#a3beeb46678aaab2565d0a2669c03ea1f',1,'data::STRING()'],['../stack_8h.html#a0bc6723d5d4776ab7cdc646728b775f7aee847e634a4297b274316de8a8ca9921',1,'STRING():&#160;stack.h']]],
+  ['subtrai_47',['subtrai',['../maths_8c.html#a90cd2dffdaa4518db18fc9804904e16e',1,'subtrai(STACK *s):&#160;maths.c'],['../maths_8h.html#a90cd2dffdaa4518db18fc9804904e16e',1,'subtrai(STACK *s):&#160;maths.c']]]
+];

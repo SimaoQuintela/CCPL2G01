dc00f34b20584843a4cd53419ae66ac9f0399d0c
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-16 21:17:36 +0100

    Delete files_0.js

--- files_0.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['main_2ec_58',['main.c',['../main_8c.html',1,'']]],
-  ['manipulacao_5fda_5fstack_2ec_59',['manipulacao_da_stack.c',['../manipulacao__da__stack_8c.html',1,'']]],
-  ['manipulacao_5fda_5fstack_2eh_60',['manipulacao_da_stack.h',['../manipulacao__da__stack_8h.html',1,'']]],
-  ['maths_2ec_61',['maths.c',['../maths_8c.html',1,'']]],
-  ['maths_2eh_62',['maths.h',['../maths_8h.html',1,'']]]
-];

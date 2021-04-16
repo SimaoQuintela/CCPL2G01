ff9b0d916ac33f035eb4f8a50331388ef7acad52
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-16 21:14:48 +0100

    Delete all_a.js

--- all_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rodatres_38',['rodatres',['../manipulacao__da__stack_8c.html#a8d6e47ebf2e0f23fdaa5f791d9ba84da',1,'rodatres(STACK *s):&#160;manipulacao_da_stack.c'],['../manipulacao__da__stack_8h.html#a8d6e47ebf2e0f23fdaa5f791d9ba84da',1,'rodatres(STACK *s):&#160;manipulacao_da_stack.c']]]
-];

4b0c856980017ec256e5927ccc686ecfe2893538
Nuno Costa <75766357+Nuno23C@users.noreply.github.com>
2021-04-25 22:17:53 +0100

    Update maths.h

--- Code/Módulos/Math/maths.h
+++ Code/Módulos/Math/maths.h
@@ -15,3 +15,21 @@ void decrementa(STACK *s);
 
 
 void incrementa(STACK *s);
+
+
+void modulo(STACK *s);
+
+
+void expoente(STACK *s);
+
+
+void e(STACK *s); 
+
+
+void ou(STACK *s);
+  
+
+void xorr(STACK *s);
+
+
+void nott(STACK *s);

6969731e2f243b45be47635bbe43f52bd842fdfa
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-25 22:03:55 +0100

    Add files via upload

---
+++ Documentation/html/search/all_7.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['main_28',['main',['../main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.c']]],
+  ['main_2ec_29',['main.c',['../main_8c.html',1,'']]],
+  ['maior_30',['maior',['../logica_8c.html#aa7e47cdda15b522ab0f1e98dfa83e16d',1,'maior(STACK *s):&#160;logica.c'],['../logica_8h.html#aa7e47cdda15b522ab0f1e98dfa83e16d',1,'maior(STACK *s):&#160;logica.c']]],
+  ['manipulacao_5fda_5fstack_2ec_31',['manipulacao_da_stack.c',['../manipulacao__da__stack_8c.html',1,'']]],
+  ['manipulacao_5fda_5fstack_2eh_32',['manipulacao_da_stack.h',['../manipulacao__da__stack_8h.html',1,'']]],
+  ['maths_2ec_33',['maths.c',['../maths_8c.html',1,'']]],
+  ['maths_2eh_34',['maths.h',['../maths_8h.html',1,'']]],
+  ['menor_35',['menor',['../logica_8c.html#aa14f0c5f29498e8b6fe16ffa6256d7af',1,'menor(STACK *s):&#160;logica.c'],['../logica_8h.html#aa14f0c5f29498e8b6fe16ffa6256d7af',1,'menor(STACK *s):&#160;logica.c']]],
+  ['modulo_36',['modulo',['../maths_8c.html#ac47c510fef88bce38e48b07b6017c54e',1,'modulo(STACK *s):&#160;maths.c'],['../maths_8h.html#ac47c510fef88bce38e48b07b6017c54e',1,'modulo(STACK *s):&#160;maths.c']]],
+  ['multiplica_37',['multiplica',['../maths_8c.html#a1a83e81a0989cfe6e96090f7fc2d2198',1,'multiplica(STACK *s):&#160;maths.c'],['../maths_8h.html#a1a83e81a0989cfe6e96090f7fc2d2198',1,'multiplica(STACK *s):&#160;maths.c']]]
+];
---
+++ Documentation/html/search/all_f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['xorr_73',['xorr',['../maths_8c.html#a83f2bddece3797ca5d999ca6add31383',1,'xorr(STACK *s):&#160;maths.c'],['../maths_8h.html#a83f2bddece3797ca5d999ca6add31383',1,'xorr(STACK *s):&#160;maths.c']]]
+];

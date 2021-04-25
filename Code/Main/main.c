514ea3fec825507652078d55501d729a3b24ea76
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-25 22:11:37 +0100

    Update main.c

--- Code/Main/main.c
+++ Code/Main/main.c
@@ -2,17 +2,19 @@
 * @file main ficheiro que contém a função main.
 * \author Simão Quintela
 */
+
 #include <stdio.h>
 #include "parser.h"
 #include "stack.h"
-
 /**
-*	\brief Função principal do programa responsável por inicializar a stack e passar a stack e uma linha para a função parse.
+* \brief A função main inicializa a stack e as letras passando-as como parametro ao fgets que é passado como parametro juntamente com a stack e as letras para o parse.
 */
 int main() {
-  char buf[BUFSIZ];       // linha que vai ser lida, com o tamanho BUFSIZ(macro pré-definida)
+  char buf[BUFSIZ];
   STACK *s = new_stack();
+  STACK *letras = new_stack();
+  preencheLetras(letras);
   
-  parse(s, fgets(buf, BUFSIZ, stdin));
+  parse(s, letras, fgets(buf, BUFSIZ, stdin));
   return 0;
 }

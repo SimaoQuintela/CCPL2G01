f8a2d54d9920e9bf10bdc5fdc76bef0476cad6f4
samuelv07 <79595267+samuelv07@users.noreply.github.com>
2021-04-25 22:15:48 +0100

    Add files via upload

---
+++ Code/Módulos/Math/maths.h
@@ -0,0 +1,17 @@
+#include "stack.h"
+
+void soma(STACK *s);
+
+void subtrai(STACK *s);
+
+
+void multiplica(STACK *s);
+    
+
+void dividir(STACK *s);
+  
+
+void decrementa(STACK *s);
+
+
+void incrementa(STACK *s);

b09f12ae13f87231753a6ba35488cfb318192b17
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-04-25 22:16:07 +0100

    Update tokenizador.h

--- Code/Parser/tokenizador.h
+++ Code/Parser/tokenizador.h
@@ -1,2 +1,5 @@
-void tokenizador(STACK *s, char* token);
+void tokenizador(STACK *s, STACK *letras, char* token);
 int verifica_carater(char carater);
+int verifica_string(char token[]);
+int verifica_variavel(char token);
+void executa_funcao_aritmetica(char carater, STACK *s, STACK *letras);

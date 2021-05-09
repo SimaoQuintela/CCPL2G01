7fd3b77898579bcfff0b06177ab64e92da210428
Tiago Guedes <79611898+guedes674@users.noreply.github.com>
2021-05-09 22:49:50 +0100

    Update tokenizador.h

--- Code/Parser/tokenizador.h
+++ Code/Parser/tokenizador.h
@@ -3,3 +3,5 @@ int verifica_carater(char carater);
 int verifica_string(char token[]);
 int verifica_variavel(char token);
 void executa_funcao_aritmetica(char carater, STACK *s, STACK *letras);
+int verifica_simbolos(char carater);
+void tokenizador_strings(STACK *s, char* token);
